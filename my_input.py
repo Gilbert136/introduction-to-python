43ca14fb0a0b76fe546a9c7f03cb964da4467b57
Gilbert <galkade@hotmail.com>
2017-02-22 15:14:48 -0600

    Intro: My first git commit

---
+++ my_input.py
@@ -0,0 +1,11 @@
+## Template
+#!/usr/bin/env python3
+
+# Name: <Gilbert Kwame Adu>
+# Index number: <PS/ITC/14/0006>
+
+# TODO: Put your codes here ...
+
+
+myinput = input('Please Enter something: ')
+print(myinput)
